69086c1d79d4294ec7cb3709ec4bfb7188a9166a
utopi <utopi@JUANTO>
2025-05-16 11:07:11 +0200

    add comment

--- use/src/gui/org/tzi/use/gui/mvm/MVMObjCheckState.java
+++ use/src/gui/org/tzi/use/gui/mvm/MVMObjCheckState.java
@@ -1059,7 +1059,7 @@ public class MVMObjCheckState extends JDialog {
 		}
 		contentNew=sourceNew;
 	}
-	//---
+	//--- from metrics
 	private void prepareContentNewFromMetrics(String newBody, MClassInvariant oInvTab) {
 
 		String sourceNew=contentFile;
